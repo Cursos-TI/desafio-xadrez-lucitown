c654f685280a4a3b0d3de2cb903e11e674844d09
lucitown <luc.math.lm62@gmail.com>
2025-08-18 15:16:02 -0300

    Update Xadrez_matriz.c
    Implementei a rainha

--- Xadrez_matriz.c
+++ Xadrez_matriz.c
@@ -10,7 +10,7 @@
 #define BISPO  'B'
 #define CAVALO 'C' 
 #define RAINHA 'R'
-#define REI    'E'  
+#define REI    'K'  
 #define PEAO   'P'
 
 char tabuleiro[TAM][TAM]; //aqui é a definição do tabuleiro. Usei char para fazer a representação com letras
@@ -77,8 +77,6 @@ int mover_torre(int x1, int y1, int x2, int y2){ //verifica se escolheu a torre,
     }
     
     if(x1 != x2 && y1 != y2){ //verifica se escolheu uma posição válida pra torre. estou confuso com o operador lógico
-        return 1; 
-    }else{
         printf("A torre so se move em linha reta.\n");
         return 0;
     }
@@ -102,10 +100,103 @@ int mover_torre(int x1, int y1, int x2, int y2){ //verifica se escolheu a torre,
             }
         }
     }
+
     tabuleiro[x2][y2] = TORRE; //movimento propriamente dito
     tabuleiro[x1][y1] = VAZIO;
     printf("Torre movida para a posição (%d,%d)\n", x2, y2);
     return 1;
+}
+
+int mover_bispo(int x1, int y1, int x2, int y2){
+    if(tabuleiro[x1][y1] != BISPO){              
+       printf("Não há um bispo nessa posição (%d,%d)\n", x1, y1);
+       return 0;
+    }
+
+    if(!valido(x2,y2)){                       //não foi uma boa ideia usar a palavra "valido" pra verificar se 
+    printf("Posição de destino inválida.\n"); //o movimento ta dentro do tabuleiro,
+    return 0;                                 //na próxima tentativa é melhor usar outra palavra
+    }
+
+    if(abs(x1 - x2) != abs(y1 - y2)){                  //Aqui precisei usar uma estrutura diferente pra validar o movimento do bispo
+        printf("O Bispo só se move na diagonal.\n");   //infelizmente ainda estou confuso com o operador lógico
+        return 0;                                      //pq não usar só o oposto do validador da torre? 
+                                                       //resposta: o bispo tem restrições diferentes, a melhor opção é comparar o módulo
+    }                                                  //das diferenças
+
+    if(y1 == y2 && x1 != x2){ //verifica se a posição diagonal ta bloqueada
+        int dx = (x2>x1) ? 1: -1;
+        int dy = (y2>y1) ? 1: -1;
+        int x = x1 + dx;
+        int y = y1 + dy;
+        while(x != x2 && y != y2){
+            if(tabuleiro[x][y] != VAZIO){
+                printf("Posição bloqueada (%d,%d).\n", x, y1);
+                return 0;
+            }
+            x += dx;
+            y += dy;
+        }
+    }
+
+    tabuleiro[x2][y2] = BISPO; //movimento propriamente dito
+    tabuleiro[x1][y1] = VAZIO;
+    printf("Bispo movido para a posição (%d,%d)\n", x2, y2);
+    return 1;
+
+}
+
+int mover_rainha(int x1, int y1, int x2, int y2){
+    if(tabuleiro[x1][y1] != RAINHA){ 
+       printf("Não há uma Rainha nessa posição (%d,%d)\n", x1, y1);
+       return 0;
+    }
+
+    if(!valido(x2,y2)){ //verifica se a posição ta dentro do tabuleiro, puxa a função ali de cima.
+    printf("Posição de destino inválida.\n");
+    return 0;
+    }
+
+    if(x1 == x2 && y1 != y2){ //verifica se a posição horizontal ta bloquada 
+        int dy = (y2>y1) ? 1: -1;
+        for(int y = y1 + dy; y != y2; y += dy){
+            if(tabuleiro[x1][y] != VAZIO){
+                printf("Posição bloqueada (%d,%d).\n", x1, y);
+                return 0;
+            }
+        }
+    }
+    
+    if(y1 == y2 && x1 != x2){ //verifica se a posição vertical ta bloquada
+        int dx = (x2>x1) ? 1: -1;
+        for(int x = x1 + dx; x != x2; x += dx){
+            if(tabuleiro[x][y1] != VAZIO){
+                printf("Posição bloqueada (%d,%d).\n", x, y1);
+                return 0;
+            }
+        }
+    }
+
+    if(y1 == y2 && x1 != x2){ //verifica se a posição diagonal ta bloqueada
+        int dx = (x2>x1) ? 1: -1;
+        int dy = (y2>y1) ? 1: -1;
+        int x = x1 + dx;
+        int y = y1 + dy;
+        while(x != x2 && y != y2){
+            if(tabuleiro[x][y] != VAZIO){
+                printf("Posição bloqueada (%d,%d).\n", x, y1);
+                return 0;
+            }
+            x += dx;
+            y += dy;
+        }
+    }
+
+    tabuleiro[x2][y2] = RAINHA; //movimento propriamente dito
+    tabuleiro[x1][y1] = VAZIO;
+    printf("Rainha movido para a posição (%d,%d)\n", x2, y2);
+    return 1;
+
 }
 
 //usar switch dentro de switch é a melhor opção? analisar isso.
@@ -115,7 +206,10 @@ void escolher_peça(){
     int peca;
     do{
     printf("Escolha a peça.\n");
+    printf("0. Voltar ao menu principal.\n");
     printf("1. Torre.\n");
+    printf("2. Bispo.\n");
+    printf("3. Rainha.\n");
     scanf("%d", &peca);
     switch (peca){
     case 1:{
@@ -136,13 +230,51 @@ void escolher_peça(){
             printf("Falha no movimento.\n");
         }
         imprimir_tabuleiro();
+    }
     break;
+    case 2:{
+        int x1, y1, x2, y2;
+        printf("Movimento do Bispo.\n");
+        printf("Digite a posição inicial do Bispo.\n");
+        scanf("%d %d", &x1, &y1);
+        printf("Digite a posição de destino.\n");
+        scanf("%d %d", &x2, &y2);
+        if(mover_bispo(x1, y1, x2, y2)){
+            #ifdef _WIN32
+                system("cls");
+            #else
+                system("clear");
+            #endif
+                printf("Movimento realizado.\n");
+        }else{
+            printf("Falha no movimento.\n");
+        }
+        imprimir_tabuleiro();
+    }
+    break;
+    case 3:
+        int x1, y1, x2, y2;
+        printf("Movmento da Rainha.\n");
+        printf("Digite a posição inicial da Rainha.\n");
+        scanf("%d %d", &x1, &y1);
+        printf("Digite a posição de destino.\n");
+        scanf("%d %d", &x2, &y2);
+        if(mover_rainha(x1, y1, x2, y2)){
+            #ifdef _WIN32
+                system("cls");
+            #else
+                system("clear");
+            #endif
+                printf("Movimento realizado.\n");
+        }else{
+            printf("Falha no movimento.\n");
+        }
+        imprimir_tabuleiro();
     case 0:
         printf("Voltando ao menu.\n");
     break;
     default:
     break;
-        }
     }
 }while(peca != 0);
 }
@@ -150,7 +282,7 @@ void escolher_peça(){
 int main(){
     int xadrez;
     do{
-    printf("=== XADREZ - TABULEIRO INICIAL ===\n\n");
+    printf("XADREZ\n\n");
     printf("1. iniciar Xadrez\n");
     printf("2. Regras.\n");
     printf("3. Sair do jogo.\n");
